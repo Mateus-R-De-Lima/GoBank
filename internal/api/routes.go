183d4a0807db0389596e70ea6e249ab3694e5bcc
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-09-07 16:30:31 -0300

    Criando as rotas de Signup e Login para disponibilizar os endpoints de autenticação e cadastro de usuários na API.

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -2,8 +2,18 @@ package api
 
 import (
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 func (a *Api) BindRoutes() {
 	a.Router.Use(middleware.RequestID, middleware.Recoverer, middleware.Logger)
+
+	a.Router.Route("/api", func(r chi.Router) {
+		r.Route("/v1", func(r chi.Router) {
+			r.Route("/users", func(r chi.Router) {
+				r.Post("/signup", a.handleSignupUser)
+				r.Post("/login", a.handleLoginUser)
+			})
+		})
+	})
 }
---
+++ internal/api/user_handlers.go
@@ -0,0 +1,6 @@
+package api
+
+import "net/http"
+
+func (a *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {}
+func (a *Api) handleLoginUser(w http.ResponseWriter, r *http.Request)  {}
