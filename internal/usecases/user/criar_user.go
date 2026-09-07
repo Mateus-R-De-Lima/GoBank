a4bec29b4cecb446122524b20d1faa136d521e50
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-09-07 18:18:07 -0300

    Implementando os casos de uso de criação de usuários e autenticação, contemplando o cadastro de novos usuários e o fluxo de login.

---
+++ internal/usecases/user/criar_user.go
@@ -0,0 +1,26 @@
+package user
+
+import (
+	"context"
+	"gobank/internal/validador"
+)
+
+type CriarUserRequest struct {
+	UserName string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (req CriarUserRequest) Valid(ctx context.Context) validador.Avaliador {
+	var a validador.Avaliador
+
+	a.VerificarCampo(validador.NaoPoderEstaEmBranco(req.UserName), "username", "UserName não pode esta em branco")
+	a.VerificarCampo(validador.NaoPoderEstaEmBranco(req.Password), "password", "Password não pode esta em branco")
+	a.VerificarCampo(validador.NaoPoderEstaEmBranco(req.Email), "email", "Email não pode esta em branco")
+
+	a.VerificarCampo(validador.MinimoCaracteres(req.Password, 8) && validador.MaximoCaracteres(req.Password, 255), "passwoerd", "Password deve ter um comprimento entre 8 e 255 ")
+	a.VerificarCampo(validador.MinimoCaracteres(req.UserName, 3) && validador.MaximoCaracteres(req.Password, 50), "passwoerd", "Password deve ter um comprimento entre 3 e 50 ")
+
+	a.VerificarCampo(validador.Corresponde(req.Email, validador.EmailRX), "email", "Email não é valido!")
+	return a
+}
