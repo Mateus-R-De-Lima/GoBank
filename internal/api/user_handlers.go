fcff2908f0cf504faf9930db5ea9746e94c7fb5b
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-09-08 21:41:14 -0300

    Implementando os handlers handleLoginUser e handleLogoutUser para realizar o login e logout do usuário.

--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -40,5 +40,57 @@ func (a *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
 		"id": id,
 	})
 }
-func (a *Api) handleLoginUser(w http.ResponseWriter, r *http.Request)   {}
-func (a *Api) handlerLogoutUser(w http.ResponseWriter, r *http.Request) {}
+func (a *Api) handleLoginUser(w http.ResponseWriter, r *http.Request) {
+	data, problemas, err := utils.DecodificarJson[user.LoginUserRequest](r)
+
+	if err != nil {
+		utils.CodificarJson(w, r, http.StatusUnprocessableEntity, problemas)
+	}
+
+	id, err := a.UserService.AuthenticarUsuario(r.Context(), data.Email, data.Password)
+
+	if err != nil {
+		if errors.Is(err, usuario.ErrorCredenciasInvalidas) {
+			utils.CodificarJson(w, r, http.StatusUnauthorized, map[string]any{
+				"error": usuario.ErrorCredenciasInvalidas,
+			})
+			return
+		}
+
+		_ = utils.CodificarJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "Erro  internoao realizar o login.",
+		})
+
+		return
+	}
+
+	err = a.Sessions.RenewToken(r.Context())
+
+	if err != nil {
+		_ = utils.CodificarJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "Erro interno ao realizar o login.",
+		})
+
+		return
+	}
+
+	a.Sessions.Put(r.Context(), "AuthenticatedUserId", id)
+	utils.CodificarJson(w, r, http.StatusOK, map[string]any{
+		"mensagem": "login realizado com sucesso",
+	})
+}
+func (a *Api) handlerLogoutUser(w http.ResponseWriter, r *http.Request) {
+	err := a.Sessions.RenewToken(r.Context())
+
+	if err != nil {
+		utils.CodificarJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "Erro interno ao realizar o logout",
+		})
+		return
+	}
+
+	a.Sessions.Remove(r.Context(), "AuthenticatedUserId")
+	utils.CodificarJson(w, r, http.StatusOK, map[string]any{
+		"mensagem": "logout realizado com sucesso",
+	})
+}
