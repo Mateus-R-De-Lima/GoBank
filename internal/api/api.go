5fff54816b4bfabf18df0ea9d8be652c493ecb5f
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-09-08 21:30:11 -0300

    Implementando o handleSignupUser, utilizando o User UseCase para validar os dados da requisição e, em seguida, o User Service para realizar o cadastro do usuário no banco de dados, retornando o ID do usuário em caso de sucesso.

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,11 +1,14 @@
 package api
 
 import (
+	"gobank/internal/services/user"
+
 	"github.com/alexedwards/scs/v2"
 	"github.com/go-chi/chi/v5"
 )
 
 type Api struct {
-	Router   *chi.Mux
-	Sessions *scs.SessionManager
+	Router      *chi.Mux
+	Sessions    *scs.SessionManager
+	UserService user.UserSerivce
 }
--- internal/api/user_handlers.go
+++ internal/api/user_handlers.go
@@ -1,7 +1,44 @@
 package api
 
-import "net/http"
+import (
+	"errors"
+	usuario "gobank/internal/services/user"
+	"gobank/internal/usecases/user"
+	"gobank/internal/utils"
+	"net/http"
+)
 
-func (a *Api) handleSignupUser(w http.ResponseWriter, r *http.Request)  {}
+func (a *Api) handleSignupUser(w http.ResponseWriter, r *http.Request) {
+	data, problemas, err := utils.DecodificarJson[user.CriarUserRequest](r)
+
+	if err != nil {
+		_ = utils.CodificarJson(w, r, http.StatusUnprocessableEntity, problemas)
+	}
+
+	id, err := a.UserService.CriarUsuario(
+		r.Context(),
+		data.UserName,
+		data.Email,
+		data.Password,
+	)
+
+	if err != nil {
+		if errors.Is(err, usuario.ErroEmailOuUserNameJaExiste) {
+			_ = utils.CodificarJson(w, r, http.StatusBadRequest, map[string]any{
+				"error": usuario.ErroEmailOuUserNameJaExiste,
+			})
+			return
+		}
+
+		_ = utils.CodificarJson(w, r, http.StatusInternalServerError, map[string]any{
+			"error": "Erro ao criar o usuario.",
+		})
+		return
+	}
+
+	_ = utils.CodificarJson(w, r, http.StatusCreated, map[string]any{
+		"id": id,
+	})
+}
 func (a *Api) handleLoginUser(w http.ResponseWriter, r *http.Request)   {}
 func (a *Api) handlerLogoutUser(w http.ResponseWriter, r *http.Request) {}
