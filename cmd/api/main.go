0804876fc239f177fcb36935c63167055fc3a163
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-09-08 21:49:32 -0300

    Carregando as variáveis de ambiente e configurando a conexão com o banco de dados.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Ola")
+
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		os.Getenv("GOBANK_DATABASE_USER"),
+		os.Getenv("GOBANK_DATABASE_PASSWORD"),
+		os.Getenv("GOBANK_DATABASE_HOST"),
+		os.Getenv("GOBANK_DATABASE_PORT"),
+		os.Getenv("GOBANK_DATABASE_NAME")))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
 }
