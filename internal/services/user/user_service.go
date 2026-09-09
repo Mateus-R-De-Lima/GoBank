57652849b4ba917db823db764d2e520deea2f8e1
Mateus-R-De-Lima <limamateus67@gmail.com>
2026-09-08 21:04:05 -0300

    Criando a função AutenticarUsuario para verificar se o usuário pode ser autenticado ou não, validando suas credenciais.

--- internal/services/user/user_service.go
+++ internal/services/user/user_service.go
@@ -6,6 +6,7 @@ import (
 	"gobank/internal/store/pgstore"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"golang.org/x/crypto/bcrypt"
@@ -53,4 +54,24 @@ func (us *UserSerivce) CriarUsuario(ctx context.Context, nome, email, password s
 	}
 
 	return id, nil
+}
+
+func (us *UserSerivce) AuthenticarUsuario(ctx context.Context, email, password string) (uuid.UUID, error) {
+	user, err := us.queries.GetUserByEmail(ctx, email)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return uuid.UUID{}, ErrorCredenciasInvalidas
+		}
+		return uuid.UUID{}, err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+
+	if err != nil {
+		return uuid.UUID{}, ErrorCredenciasInvalidas
+	}
+
+	return user.ID, nil
+
 }
